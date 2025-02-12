0c3e825b9f60b3c03e83bd8b87b6ca14e2b0e899
AHRUMPARK <arum4523@naver.com>
2025-02-12 12:07:22 +0900

    fisrt commit

---
+++ app/ui/font.ts
@@ -0,0 +1,17 @@
+import { Inter, Lusitana } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+
+export const lustiana = Lusitana({ weight: ["400", "700"] });
+
+// .lusitana-regular {
+//   font-family: "Lusitana", serif;
+//   font-weight: 400;
+//   font-style: normal;
+// }
+
+// .lusitana-bold {
+//   font-family: "Lusitana", serif;
+//   font-weight: 700;
+//   font-style: normal;
+// }
