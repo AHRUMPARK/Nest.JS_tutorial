6323cd0a8d5ae0ec8497137b1cbacaf9cbf10a55
AHRUMPARK <arum4523@naver.com>
2025-02-12 12:16:13 +0900

    폰트 오류 수정

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -2,7 +2,11 @@ import { Inter, Lusitana } from "next/font/google";
 
 export const inter = Inter({ subsets: ["latin"] });
 
-export const lustiana = Lusitana({ weight: ["400", "700"] });
+export const lusitana = Lusitana({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+  preload: true,
+});
 
 // .lusitana-regular {
 //   font-family: "Lusitana", serif;
